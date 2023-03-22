38bc4c947980849ca82b220ac433d028d1d01811
zj <zj@qq.com>
2023-03-22 22:59:51 +0800

    测试提交44

--- ignite-mybatisyplus-demo/src/test/java/com/zj/service/ListProcessor.java
+++ ignite-mybatisyplus-demo/src/test/java/com/zj/service/ListProcessor.java
@@ -69,7 +69,7 @@ public class ListProcessor {
         // 输出结果
         System.out.println(resultList);
 
-        // 关闭ExecutorService 111
+        // 关闭ExecutorService 111 adfas
         executor.shutdown();
     }
 }
